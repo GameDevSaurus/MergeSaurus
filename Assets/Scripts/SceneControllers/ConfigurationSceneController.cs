3c928abffeaa5d0986f65ba65855713f49e7d7da
torcu <pablotorcuato@gmail.com>
2020-06-22 15:47:10 +0200

    Configuration Scene Is Working!

--- Assets/Scripts/SceneControllers/ConfigurationSceneController.cs
+++ Assets/Scripts/SceneControllers/ConfigurationSceneController.cs
@@ -4,20 +4,49 @@ using UnityEngine;
 
 public class ConfigurationSceneController : MonoBehaviour
 {
-    public TimeController tController;
-
+    public TimeController _timeController;
+    public float barFillDuration = 2f;
+    float _loadAmount;
+    bool _waitForTime = false;
     // Start is called before the first frame update
     void Start()
     {
         if (!UserDataController.Exist())
         {
-            UserDataController.InitializeUser(tController.GetTimeNow().ToBinary().ToString());
-            GameEvents.LoadScene.Invoke("Main");
+            _waitForTime = true;
         }
         else
         {
             UserDataController.LoadFromFile();
-            GameEvents.LoadScene.Invoke("Main");
+            StartCoroutine(LoadingBar());
+        }   
+    }
+
+    public void Update()
+    {
+        if (_waitForTime)
+        {
+            if (_timeController._timeChecked)
+            {
+                UserDataController.InitializeUser(_timeController.GetTimeNow().ToBinary().ToString());
+                _waitForTime = false;
+                StartCoroutine(LoadingBar());
+            }
         }
+    }
+    public IEnumerator LoadingBar()
+    {
+        for(float i = 0; i < barFillDuration; i += Time.deltaTime)
+        {
+            yield return null;
+           _loadAmount = i / barFillDuration;
+        }
+        _loadAmount = 1f;
+        GameEvents.LoadScene.Invoke("MainGame");
+    }
+
+    public float GetLoadAmount()
+    {
+        return _loadAmount;
     }
 }
