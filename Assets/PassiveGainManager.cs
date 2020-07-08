978f25b50d9d37b9ecb7bbb342800095129190fa
torcu <45042365+Exater5@users.noreply.github.com>
2020-07-08 19:59:43 +0200

    New UI Panels

---
+++ Assets/PassiveGainManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassiveGainManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject _mainPanel;
+
+    public void OpenVip()
+    {
+        _mainPanel.SetActive(true);
+    }
+    public void CloseVip()
+    {
+        _mainPanel.SetActive(false);
+    }
+}
---
+++ Assets/VipManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VipManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject _mainPanel;
+
+    public void OpenVip()
+    {
+        _mainPanel.SetActive(true);
+    }
+    public void CloseVip()
+    {
+        _mainPanel.SetActive(false);
+    }
+}
