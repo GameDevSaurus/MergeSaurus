2848b0c25add7f52a5f07a10a76876a524f68f53
MHernandezOlmo <60235796+MHernandezOlmo@users.noreply.github.com>
2020-07-16 13:39:33 +0200

    LA PULPOS

--- Assets/Scripts/DinosaurInstance.cs
+++ Assets/Scripts/DinosaurInstance.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DragonBones;
 
 public class DinosaurInstance : MonoBehaviour
 {
@@ -17,14 +18,45 @@ public class DinosaurInstance : MonoBehaviour
     {
         _working = true;
         GameEvents.WorkDino.Invoke();
-        _dinoSprite.color = new Color(95f / 255f, 95f / 255f, 95f / 255f, 1f);
-        _dinoAnimator.speed = 0f;
+        if (_dinoSprite != null)
+        {
+            _dinoSprite.color = new Color(95f / 255f, 95f / 255f, 95f / 255f, 1f);
+        }
+        
+        if(_dinoAnimator != null)
+        {
+            _dinoAnimator.speed = 0f;
+        }
+        else
+        {
+            UnityArmatureComponent armature = GetComponentInChildren<UnityArmatureComponent>();
+            if (armature != null)
+            {
+                armature.animation.timeScale = 0;
+            }
+        }
+        
     }
     public void StopWorking()
     {
         _working = false;
-        _dinoSprite.color = Color.white;
-        _dinoAnimator.speed = 1f;
+        if (_dinoSprite != null)
+        {
+            _dinoSprite.color = Color.white;
+        }
+        
+        if (_dinoAnimator != null)
+        {
+            _dinoAnimator.speed = 1f;
+        }
+        else
+        {
+            UnityArmatureComponent armature = GetComponentInChildren<UnityArmatureComponent>();
+            if (armature != null)
+            {
+                armature.animation.timeScale = 1f;
+            }
+        }
     }
 
     public void SetCell(int nCell)
