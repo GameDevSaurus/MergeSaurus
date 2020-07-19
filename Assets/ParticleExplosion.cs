d7fb04a9494f2444e139ca2064d70dc3a2e82aba
MHernandezOlmo <60235796+MHernandezOlmo@users.noreply.github.com>
2020-07-19 22:19:33 +0200

    Updated Vfx

---
+++ Assets/ParticleExplosion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleExplosion : MonoBehaviour
+{
+    [SerializeField]
+    ParticleSystem _confettiRain;
+    [SerializeField]
+    ParticleSystem _confettiExplosion;
+    [SerializeField]
+    ParticleSystem _implosion;
+    [SerializeField]
+    ParticleSystem _explosion;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
