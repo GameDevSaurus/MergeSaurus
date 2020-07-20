b13fe08f780c803f7129e0d020605a712463ff47
torcu <45042365+Exater5@users.noreply.github.com>
2020-07-20 12:57:26 +0200

    Reward Instance Config

--- Assets/DailyRewardInstance.cs
+++ Assets/DailyRewardInstance.cs
@@ -7,14 +7,22 @@ public class DailyRewardInstance : MonoBehaviour
 {
     [SerializeField]
     Image background, upperBorder, lowerborder, button, buttonBorder;
-
+    [SerializeField]
+    Color bgC1, bgC2, bordersC1, bordersC2, buttonC1, buttonC2, buttonBorderC1, buttonBorderC2;
     public void BasicConfig()
     {
-
+        background.color = bgC1;
+        upperBorder.color = bordersC1;
+        lowerborder.color = bordersC1;
+        button.color = buttonC1;
+        buttonBorder.color = buttonBorderC1;
     }
-
     public void SelectedConfig()
     {
-
+        background.color = bgC2;
+        upperBorder.color = bordersC2;
+        lowerborder.color = bordersC2;
+        button.color = buttonC2;
+        buttonBorder.color = buttonBorderC2;
     }
 }
